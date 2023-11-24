e031ffc6f45164d07523c6dce49815233eb12bbe
Kay <info@beckmann-md.de>
2023-11-24 20:16:35 +0100

    add allowed hosts

--- todolistbackend/settings.py
+++ todolistbackend/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-cb!)3#q_t+s-i!tqg&xyw$8ryc-w)v266iuzae(g11f+n0-u6&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '10.10.0.22'
+]
 
 
 # Application definition
