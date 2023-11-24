c6c40fef1d8ce9ae139b2b2d9630f0d284c34fe8
Kay <info@beckmann-md.de>
2023-11-24 20:36:38 +0100

    add middelware an allow all hosts

--- todolistbackend/settings.py
+++ todolistbackend/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = [
     '10.10.0.22'
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 # Application definition
 
@@ -47,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
