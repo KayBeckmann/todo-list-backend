ebae8b203ae948279d932dd78c5e35045cd9c8d8
Kay <info@beckmann-md.de>
2023-12-02 22:48:02 +0100

    add serializer for author

--- todolist/serializers.py
+++ todolist/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from .models import TodoItem
 
+class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+  class Meta:
+    model = User
+    fields = ['id', 'username']
+
 class TodoItemSerializer(serializers.ModelSerializer):
   class Meta:
     model = TodoItem
